bea5a90d8e0bf9c48a374a5c7e36bf6317cbdb61
olmae-strike <65939875+olmae-strike@users.noreply.github.com>
2020-11-23 14:41:43 +0900

    modified

--- zinnotech/Program.cs
+++ zinnotech/Program.cs
@@ -7,6 +7,7 @@ namespace zinnotech
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Zinnotech!");
+            Console.WriteLine("Hello Unity!");
         }
     }
 }
